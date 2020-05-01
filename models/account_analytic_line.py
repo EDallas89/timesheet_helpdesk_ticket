31565137b3916874e1492920907cf4100144855c
cramos <cramos@puntsistemes.es>
2020-05-01 19:38:22 +0200

    [FIX] Solucionado fallo calculo horas computadas. Reestyling vista

--- models/account_analytic_line.py
+++ models/account_analytic_line.py
@@ -2,6 +2,7 @@ from odoo import models, fields, api
 from datetime import datetime
 import math
 
+
 class AccountAnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
     _order = 'date_start asc'
@@ -11,84 +12,78 @@ class AccountAnalyticLine(models.Model):
         string='ticket_id',
     )
     # Campos para la Vista Tree
-    start_stop = fields.Boolean(string='Start Stop', default=False)
-    date_start = fields.Datetime('Start Time')
-    date_stop = fields.Datetime('End Time')
-    date_reboot = fields.Datetime('Reboot Time')
-    computed_hours = fields.Float('Computed Hours')
+    start_stop = fields.Boolean(
+        string='Start Stop'
+    )
+    date_start = fields.Datetime(
+        string='Start Time'
+    )
+    date_stop = fields.Datetime(
+        string='End Time'
+    )
+    date_reboot = fields.Datetime(
+        string='Reboot Time'
+    )
+    computed_hours = fields.Float(
+        string='Computed Hours'
+    )
 
-    # Cambia el valor de start_stop a True y escribe la fecha y hora actual en el campo date_start. Si el campo date_start ya tiene valor no lo modifica.
     @api.multi
     def action_start(self):
         date_start = self.date_start
-        if (date_start==False):
-            return self.write({'date_start':datetime.now(), 'start_stop':True})
+        if date_start == False:
+            return self.write({'date_start': datetime.now(), 'start_stop': True})
         else:
-            return self.write({'date_pause': datetime.now(), 'start_stop':True})
-                
-    # Cuenta las horas y minutos entre dos fechas. Si hay una fecha de reinicio tomará esta, sino utilizará la fecha de inicio
+            return self.write({'date_pause': datetime.now(), 'start_stop': True})
+
     @api.multi
     def count_time(self):
         date_reboot = self.date_reboot
-        # Si hay fecha de reinicio calcula de diferencia entre ella y la fecha actual y resetea la fecha de reinicio
-        if (date_reboot!=False):
-            datetime_diff = datetime.now() - self.date_reboot
-            self.date_reboot=False
-        # Si no hay fecha de reinicio calcula la diferencia entre la fecha de incio y la fecha actual
+        if date_reboot:
+            datetime_diff = datetime.now() - date_reboot
+            self.date_reboot = False
         else:
-        # Comparamos la fecha actual con la fecha de Start, convirtiendola en String
             datetime_diff = datetime.now() - self.date_start
-
-        # Convertimos los segundos totales entre Start y Stop en minutos
         minutes, seconds = divmod(datetime_diff.total_seconds(), 60)
-                               
+
         # Convertimos los minutos en horas
         hours, minutes = divmod(minutes, 60)
         # Damos formato a la hora
-        dur_hours = (('%0*d')%(2,hours))
-        dur_minutes = (('%0*d')%(2,minutes*1.677966102))
-        duration = dur_hours+'.'+dur_minutes
+        dur_hours = (('%0*d') % (2, hours))
+        dur_minutes = (('%0*d') % (2, minutes * 1.677966102))
+        duration = dur_hours + '.' + dur_minutes
 
         return duration
 
-    # Llama a la funbción count_time. El resultado lo graba en el campo unit_amount.
-    # Pone el campo start_stop en False.
     @api.multi
     def action_pause(self):
         duration = self.count_time()
-        # Comprueba si el valor de unit_amount es igual a computed_hours, si no es igual (porque ha sido modificado a mano) no modifica el campo.
-        if (self.unit_amount == self.computed_hours):
-            values = self.write({
-            'start_stop':False, 
-            'computed_hours':duration,
-            'unit_amount':duration,
-            })
-        else:
-             values = self.write({
-            'start_stop':False, 
-            'computed_hours':duration,
+
+        if self.unit_amount == self.computed_hours:
+            return self.write({
+                'start_stop': False,
+                'computed_hours': duration,
+                'unit_amount': duration,
             })
 
-    # Llama a la función count_time. El resultado lo graba en el campo unit_amount.
-    # Pone el campo start_stop en False y graba date_stop.
+        return self.write({
+            'start_stop': False,
+            'computed_hours': duration,
+        })
+
     @api.multi
     def action_stop(self):
         duration = self.count_time()
-        # Comprueba si el valor de unit_amount es igual a computed_hours, si no es igual (porque ha sido modificado a mano) no modifica el campo.
-        if (self.unit_amount == self.computed_hours):
-            values = self.write({
-            'start_stop':False, 
-            'date_stop':datetime.now(), 
-            'computed_hours':duration,
-            'unit_amount':duration,
-            })
-        else:
-             values = self.write({
-            'start_stop':False, 
-            'date_stop':datetime.now(), 
-            'computed_hours':duration,
+        if self.unit_amount == self.computed_hours:
+            return self.write({
+                'start_stop': False,
+                'date_stop': datetime.now(),
+                'computed_hours': duration,
+                'unit_amount': duration,
             })
 
-        return values
-
-    
+        return self.write({
+            'start_stop': False,
+            'date_stop': datetime.now(),
+            'computed_hours': duration,
+        })
--- models/ticket_timesheet.py
+++ models/ticket_timesheet.py
@@ -1,41 +1,46 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
+
 class TicketTimesheet(models.Model):
     _inherit = 'helpdesk.ticket'
 
-    project = fields.Many2one(
-        comodel_name = 'project.project',
-        string = 'project_id',
+    project_id = fields.Many2one(
+        comodel_name='project.project',
+        string='Project',
     )
-    
-    task = fields.Many2one(
-        comodel_name = 'project.task',
-        string = 'task',
+    task_id = fields.Many2one(
+        comodel_name='project.task',
+        string='Task',
     )
-
     timesheet_ids = fields.One2many(
         comodel_name='account.analytic.line',
         inverse_name='ticket_id',
         string='Timesheet',
     )
+    total_computed_hours_ticket = fields.Float(
+        compute='compute_hours',
+        string='Computed Hours'
+    )
+    partner_phone = fields.Char(
+        string='Contact Phone',
+        related='partner_id.mobile',
+        store=True,
+        readonly=False,
+    )
 
-    ##### REVISAR NO FUNCIONA #####
-    #total_computed_hours_ticket = fields.Float(computed='compute_hours')
-#
-    #@api.depends('computed_hours')
-    #def compute_hours(self):
-    #    for task in self:
-    #        task.total_computed_hours_ticket = sum(task.sudo().mapped('computed_hours'))
+    def compute_hours(self):
+        for record in self:
+            record.total_computed_hours_ticket = \
+                sum(record.mapped('timesheet_ids.computed_hours'))
 
     total_hours_ticket = fields.Float(compute='impute_hours')
 
     @api.depends('timesheet_ids.unit_amount')
     def impute_hours(self):
-        for task in self:
-            task.total_hours_ticket = sum(task.sudo().timesheet_ids.mapped('unit_amount'))
+        for record in self:
+            record.total_hours_ticket = sum(record.timesheet_ids.mapped('unit_amount'))
 
-    # Si cambia el Proyecto resetea la Tarea
     @api.onchange('project')
     def _onchange_project(self):
         self.task = False
